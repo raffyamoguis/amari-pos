51b0ec6e99233c9c7c6909f721ced4a873a97cec
raffyamoguis <raffy.amoguis1@gmail.com>
2023-08-07 23:11:49 +0800

    update

--- src/pages/Request.tsx
+++ src/pages/Request.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { Box, Card, Text } from "@mantine/core";
+import { DateInput } from "@mantine/dates";
+import { IconCalendarDue } from "@tabler/icons-react";
+
+import ListRequest from "../components/request/ListRequest";
 
 const Request: React.FC = () => {
-  return <div>Request</div>;
+  return (
+    <Box
+      sx={(theme) => ({
+        backgroundColor: theme.colors.gray[0],
+      })}
+    >
+      <Card mih="80vh">
+        <Text mb={10}>Create request</Text>
+        <DateInput
+          placeholder="Set the date"
+          maw={300}
+          icon={<IconCalendarDue size="0.80rem" />}
+          clearable
+        />
+
+        <ListRequest />
+      </Card>
+    </Box>
+  );
 };
 
 export default Request;
