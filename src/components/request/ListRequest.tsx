7581f617bba9641b4ff62e416d7d60165966bebe
raffyamoguis <raffy.amoguis1@gmail.com>
2023-08-07 23:11:38 +0800

    added request components

---
+++ src/components/request/ListRequest.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Table } from "@mantine/core";
+import { IconEye } from "@tabler/icons-react";
+
+import IconAction from "../IconAction";
+
+const ListRequest: React.FC = () => {
+  return (
+    <Table mt={10}>
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Total</th>
+          <th>Amount</th>
+          <th>Change</th>
+          <th>View</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>January 10, 1998</td>
+          <td>100</td>
+          <td>200</td>
+          <td>100</td>
+          <td>
+            <IconAction color="blue" icon={<IconEye />} />
+          </td>
+        </tr>
+        <tr>
+          <td>December 25, 1990</td>
+          <td>300</td>
+          <td>500</td>
+          <td>200</td>
+          <td>
+            <IconAction color="blue" icon={<IconEye />} />
+          </td>
+        </tr>
+        <tr>
+          <td>January 10, 2007</td>
+          <td>50</td>
+          <td>50</td>
+          <td>0</td>
+          <td>
+            <IconAction color="blue" icon={<IconEye />} />
+          </td>
+        </tr>
+      </tbody>
+    </Table>
+  );
+};
+
+export default ListRequest;
