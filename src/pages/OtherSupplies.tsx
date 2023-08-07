3e5fc85f62b73803dd1fea2276c8efa557e59456
raffyamoguis <raffy.amoguis1@gmail.com>
2023-08-07 22:07:08 +0800

    update

--- src/pages/OtherSupplies.tsx
+++ src/pages/OtherSupplies.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Box, Card } from "@mantine/core";
+
+import NewSupplies from "../components/othersupplies/NewSupplies";
+import ListNewSupplies from "../components/othersupplies/ListNewSupplies";
 
 const OtherSupplies: React.FC = () => {
-  return <div>OtherSupplies</div>;
+  return (
+    <Box
+      sx={(theme) => ({
+        backgroundColor: theme.colors.gray[0],
+      })}
+    >
+      <Card mih="80vh">
+        <NewSupplies />
+        <ListNewSupplies />
+      </Card>
+    </Box>
+  );
 };
 
 export default OtherSupplies;
