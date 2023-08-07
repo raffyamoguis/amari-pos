6b0bacb3c027fcc6d8dc2257938611224c736143
raffyamoguis <raffy.amoguis1@gmail.com>
2023-08-07 12:23:09 +0800

    added lib

---
+++ src/lib/nav/data.ts
@@ -0,0 +1,18 @@
+import {
+  IconHome2,
+  IconBuildingStore,
+  IconNurse,
+  IconBrandDeliveroo,
+  IconBuildingWarehouse,
+  IconGitPullRequest,
+} from "@tabler/icons-react";
+
+
+export const navData = [
+    {label: "Home", icon: IconHome2, route: "/"},
+    {label: "Sell", icon: IconBuildingStore, route: "/sell"},
+    {label: "Medicine", icon: IconNurse, route: "/medicine"},
+    {label: "Other Supplies", icon: IconBrandDeliveroo, route: "/othersupplies"},
+    {label: "Stocks", icon: IconBuildingWarehouse, route: "/stocks"},
+    {label: "Request", icon: IconGitPullRequest, route: "/request"},
+]
