e02283b6328736ba7cef57a00fe55899953428d0
raffyamoguis <raffy.amoguis1@gmail.com>
2023-08-07 13:37:21 +0800

    added sell components

---
+++ src/components/sell/NewTrans.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import {
+  Text,
+  TextInput,
+  NumberInput,
+  Table,
+  ActionIcon,
+  Group,
+} from "@mantine/core";
+import { IconSearch, IconTrash, IconReceipt2 } from "@tabler/icons-react";
+
+const NewTrans: React.FC = () => {
+  return (
+    <>
+      <Text mb={10}>New Transactions</Text>
+      <TextInput
+        mb={4}
+        placeholder="Enter product name"
+        icon={<IconSearch size="0.8rem" />}
+      />
+      <Table fontSize="xs">
+        <thead>
+          <tr>
+            <th>Product</th>
+            <th>Stock</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Total</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Medicol</td>
+            <td>100</td>
+            <td>5.00</td>
+            <td>
+              <NumberInput sx={{ width: "70px" }} defaultValue={0} />
+            </td>
+            <td>35.00</td>
+            <td>
+              <ActionIcon color="red" size="sm">
+                <IconTrash size="0.875rem" />
+              </ActionIcon>
+            </td>
+          </tr>
+          <tr>
+            <td>Trust</td>
+            <td>500</td>
+            <td>5.00</td>
+            <td>
+              <NumberInput sx={{ width: "70px" }} defaultValue={0} />
+            </td>
+            <td>50.00</td>
+            <td>
+              <ActionIcon color="red" size="sm">
+                <IconTrash size="0.875rem" />
+              </ActionIcon>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+      <Group mb={4}>
+        <NumberInput label="Total" defaultValue={0} hideControls disabled />
+        <NumberInput label="Change" defaultValue={0} hideControls disabled />
+        <NumberInput label="Amount" defaultValue={0} hideControls />
+      </Group>
+    </>
+  );
+};
+
+export default NewTrans;
---
+++ src/components/sell/TransHistory.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Table, Text } from "@mantine/core";
+
+const TransHistory: React.FC = () => {
+  return (
+    <>
+      <Text mb={10}>Transaction History</Text>
+      <Table>
+        <thead>
+          <tr>
+            <th>Purchase Date</th>
+            <th>Total</th>
+            <th>Amount</th>
+            <th>Change</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>January 12, 2001</td>
+            <td>300</td>
+            <td>500</td>
+            <td>200</td>
+          </tr>
+          <tr>
+            <td>January 12, 2001</td>
+            <td>300</td>
+            <td>500</td>
+            <td>200</td>
+          </tr>
+          <tr>
+            <td>January 12, 2001</td>
+            <td>300</td>
+            <td>500</td>
+            <td>200</td>
+          </tr>
+          <tr>
+            <td>January 12, 2001</td>
+            <td>300</td>
+            <td>500</td>
+            <td>200</td>
+          </tr>
+          <tr>
+            <td>January 12, 2001</td>
+            <td>300</td>
+            <td>500</td>
+            <td>200</td>
+          </tr>
+          <tr>
+            <td>January 12, 2001</td>
+            <td>300</td>
+            <td>500</td>
+            <td>200</td>
+          </tr>
+        </tbody>
+      </Table>
+    </>
+  );
+};
+
+export default TransHistory;
