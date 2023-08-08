a90c09072ed22425c9ed1b28b3723876d503a802
raffyamoguis <raffy.amoguis1@gmail.com>
2023-08-09 00:06:00 +0800

    added footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,14 @@
+import { Text } from "@mantine/core";
+import "./Footer.css"; // Import the CSS for styling
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <Text size="xs" color="gray">
+        © 2023 Raffy Amoguis
+      </Text>
+    </footer>
+  );
+}
+
+export default Footer;
