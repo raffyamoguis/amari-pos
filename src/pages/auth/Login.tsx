10d7d253b38b3b95736735442982bcedb5f7b637
raffyamoguis <raffy.amoguis1@gmail.com>
2023-08-10 17:05:38 +0800

    added login functionality

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -9,11 +9,23 @@ import {
   Stack,
 } from "@mantine/core";
 import { useAuth } from "../../util/AuthContext";
+import { useForm } from "@mantine/form";
 
 import Footer from "../../components/footer/Footer";
 
 const Login: React.FC = () => {
   const { handleUserLogin, isLoggingIn } = useAuth();
+  const form = useForm({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+
+    validate: {
+      username: (value) => (value.length === 0 ? "Username is required" : null),
+      password: (value) => (value.length === 0 ? "Username is required" : null),
+    },
+  });
   return (
     <>
       <Flex justify="center" align="center" style={{ height: "100vh" }}>
@@ -22,21 +34,24 @@ const Login: React.FC = () => {
             Amari Angels Pharmacy
           </Text>
           <Card padding="xl" py={40} miw={370} withBorder>
-            <Text fw={500}>Login</Text>
-            <TextInput mt={10} label="Username" placeholder="Enter username" />
-            <PasswordInput
-              mt={10}
-              label="Password"
-              placeholder="Enter password"
-            />
-            <Button
-              mt={20}
-              fullWidth
-              onClick={handleUserLogin}
-              loading={isLoggingIn}
-            >
-              Login
-            </Button>
+            <form onSubmit={form.onSubmit((values) => handleUserLogin(values))}>
+              <Text fw={500}>Login</Text>
+              <TextInput
+                mt={10}
+                label="Username"
+                placeholder="Enter username"
+                {...form.getInputProps("username")}
+              />
+              <PasswordInput
+                mt={10}
+                label="Password"
+                placeholder="Enter password"
+                {...form.getInputProps("password")}
+              />
+              <Button type="submit" mt={20} fullWidth loading={isLoggingIn}>
+                Login
+              </Button>
+            </form>
           </Card>
         </Stack>
       </Flex>
