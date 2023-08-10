9516cb9d3c7f5377b1ad529d2a992a6b89ac69fd
raffyamoguis <raffy.amoguis1@gmail.com>
2023-08-10 22:07:35 +0800

    added new types

--- src/types.ts
+++ src/types.ts
@@ -5,3 +5,20 @@ export interface NavTypes {
     route?: string;
     icon: React.ReactNode;
 }
+
+export interface MedicineType {
+    batchno: string;
+    name: string;
+    spefication?: number;
+    price: number;
+    expiry: string
+}
+
+export interface TransactionTypes {
+    id: number;
+    product: string;
+    stock: number;
+    price: number;
+    quantity: number;
+    total: number;
+}
