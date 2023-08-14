dfe6779d52238e2edc594021e23d2ab8ff02b0ad
raffyamoguis <raffy.amoguis1@gmail.com>
2023-08-14 23:15:26 +0800

    update

--- src/api/payments.ts
+++ src/api/payments.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { API_HOST } from "../config";
 
-export async function fetchPayments() {
+export async function fetchPayments(offset: number) {
     try {
-        const result = await axios.get(`${API_HOST}/payment`);
+        const result = await axios.get(`${API_HOST}/payment?offset=${offset}`);
         return result.data;
     }catch (error) {
         console.log("An error occured on getMedicine func: ",error);
