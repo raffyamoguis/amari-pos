02d04cca4ba625f853ec4463d19590a4012e1a51
raffyamoguis <raffy.amoguis1@gmail.com>
2023-08-14 23:15:05 +0800

    added pagination

--- src/components/sell/TransHistory.tsx
+++ src/components/sell/TransHistory.tsx
@@ -1,20 +1,36 @@
 import React from "react";
 import { useQuery } from "react-query";
-import { Table, Text } from "@mantine/core";
+import { shallow } from "zustand/shallow";
+import { Table, Text, Pagination, Center } from "@mantine/core";
 
 import SkeletonTable from "../skeleton/SkeletonTable";
 
 import { PaymentTypes } from "../../types";
 import { fetchPayments } from "../../api/payments";
+import { useTransactionHistoryStore } from "../../store/useTransactionHistoryStore";
+
+interface Props {
+  total: number;
+  items: PaymentTypes[];
+}
 
 const TransHistory: React.FC = () => {
+  const [activePage, offset, setActivePage] = useTransactionHistoryStore(
+    (state) => [state.activePage, state.offset, state.setActivePage],
+    shallow
+  );
+
   const {
     data: payments,
     isLoading,
     isSuccess,
     isError,
     error,
-  } = useQuery<PaymentTypes[], Error>("payments", fetchPayments);
+  } = useQuery<Props, Error>(
+    ["payments", offset],
+    () => fetchPayments(offset),
+    { keepPreviousData: true }
+  );
 
   if (isError) {
     console.log(error);
@@ -26,26 +42,37 @@ const TransHistory: React.FC = () => {
       {isLoading ? (
         <SkeletonTable />
       ) : isSuccess ? (
-        <Table fontSize="xs">
-          <thead>
-            <tr>
-              <th>Purchase Date</th>
-              <th>Total</th>
-              <th>Amount</th>
-              <th>Change</th>
-            </tr>
-          </thead>
-          <tbody>
-            {payments?.map((transaction: PaymentTypes) => (
+        <>
+          <Table fontSize="xs">
+            <thead>
               <tr>
-                <td>{transaction.orderdate}</td>
-                <td>{transaction.overalltotal}</td>
-                <td>{transaction.amount}</td>
-                <td>{transaction.change}</td>
+                <th>Purchase Date</th>
+                <th>Total</th>
+                <th>Amount</th>
+                <th>Change</th>
               </tr>
-            ))}
-          </tbody>
-        </Table>
+            </thead>
+            <tbody>
+              {payments?.items.map((transaction: PaymentTypes) => (
+                <tr key={transaction.id}>
+                  <td>{transaction.orderdate}</td>
+                  <td>{transaction.overalltotal}</td>
+                  <td>{transaction.amount}</td>
+                  <td>{transaction.change}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+          <Center mt={12}>
+            <Pagination
+              size="sm"
+              total={payments.total / 15}
+              siblings={1}
+              value={activePage}
+              onChange={setActivePage}
+            />
+          </Center>
+        </>
       ) : (
         <Text ta="center">{String(error)}</Text>
       )}
