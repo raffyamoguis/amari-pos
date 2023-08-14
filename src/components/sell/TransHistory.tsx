9004f83d8ee439497dba3f4420fed46d21f3baed
raffyamoguis <raffy.amoguis1@gmail.com>
2023-08-14 20:43:23 +0800

    updated trans history

--- src/components/sell/TransHistory.tsx
+++ src/components/sell/TransHistory.tsx
@@ -2,38 +2,53 @@ import React from "react";
 import { useQuery } from "react-query";
 import { Table, Text } from "@mantine/core";
 
+import SkeletonTable from "../skeleton/SkeletonTable";
+
 import { PaymentTypes } from "../../types";
 import { fetchPayments } from "../../api/payments";
 
 const TransHistory: React.FC = () => {
-  const { data: payments } = useQuery<PaymentTypes[], Error>(
-    "payments",
-    fetchPayments
-  );
+  const {
+    data: payments,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useQuery<PaymentTypes[], Error>("payments", fetchPayments);
+
+  if (isError) {
+    console.log(error);
+  }
 
   return (
     <>
       <Text mb={10}>Transaction History</Text>
-      <Table fontSize="xs">
-        <thead>
-          <tr>
-            <th>Purchase Date</th>
-            <th>Total</th>
-            <th>Amount</th>
-            <th>Change</th>
-          </tr>
-        </thead>
-        <tbody>
-          {payments?.map((transaction: PaymentTypes) => (
+      {isLoading ? (
+        <SkeletonTable />
+      ) : isSuccess ? (
+        <Table fontSize="xs">
+          <thead>
             <tr>
-              <td>{transaction.orderdate}</td>
-              <td>{transaction.overalltotal}</td>
-              <td>{transaction.amount}</td>
-              <td>{transaction.change}</td>
+              <th>Purchase Date</th>
+              <th>Total</th>
+              <th>Amount</th>
+              <th>Change</th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+            {payments?.map((transaction: PaymentTypes) => (
+              <tr>
+                <td>{transaction.orderdate}</td>
+                <td>{transaction.overalltotal}</td>
+                <td>{transaction.amount}</td>
+                <td>{transaction.change}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      ) : (
+        <Text ta="center">{String(error)}</Text>
+      )}
     </>
   );
 };
