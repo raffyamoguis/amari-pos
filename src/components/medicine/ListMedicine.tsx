b4ce83e8d4e130f3512cf5093c6ceda697bd1dec
raffyamoguis <raffy.amoguis1@gmail.com>
2023-08-14 21:18:58 +0800

    update

--- src/components/medicine/ListMedicine.tsx
+++ src/components/medicine/ListMedicine.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useQuery } from "react-query";
-import { Group, Table, TextInput, Flex, Text } from "@mantine/core";
+import {
+  Group,
+  Table,
+  TextInput,
+  Flex,
+  Text,
+  Skeleton,
+  Center,
+  Loader,
+} from "@mantine/core";
 import { modals } from "@mantine/modals";
 import { notifications } from "@mantine/notifications";
 import { IconSearch, IconEdit, IconTrash } from "@tabler/icons-react";
@@ -10,14 +19,16 @@ import { fetchMedicines } from "../../api/medicine";
 import { MedicineType } from "../../types";
 
 import IconAction from "../IconAction";
+import SkeletonTable from "../skeleton/SkeletonTable";
 
 const ListMedicine: React.FC = () => {
   const navigate = useNavigate();
 
-  const { data: medicines } = useQuery<MedicineType[], Error>(
-    "medicines",
-    fetchMedicines
-  );
+  const {
+    data: medicines,
+    isLoading,
+    isSuccess,
+  } = useQuery<MedicineType[], Error>("medicines", fetchMedicines);
 
   const openDeleteModal = () =>
     modals.openConfirmModal({
@@ -47,43 +58,47 @@ const ListMedicine: React.FC = () => {
           icon={<IconSearch size="0.8rem" />}
         />
       </Group>
-      <Table fontSize="xs">
-        <thead>
-          <tr>
-            <th>Batch no</th>
-            <th>Name</th>
-            <th>Specification</th>
-            <th>Price</th>
-            <th>Expiry</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {medicines?.map((medicine: MedicineType) => (
-            <tr key={medicine.id}>
-              <td>{medicine.batchno}</td>
-              <td>{medicine.name}</td>
-              <td>{medicine.spefication}</td>
-              <td>{medicine.price}</td>
-              <td>{medicine.expiry}</td>
-              <td>
-                <Flex>
-                  <IconAction
-                    color="green"
-                    icon={<IconEdit size="1.125rem" />}
-                    onClick={() => navigate(`/medicine/${10}`)}
-                  />
-                  <IconAction
-                    color="red"
-                    icon={<IconTrash size="1.125rem" />}
-                    onClick={openDeleteModal}
-                  />
-                </Flex>
-              </td>
+      {isLoading ? (
+        <SkeletonTable />
+      ) : (
+        <Table fontSize="xs">
+          <thead>
+            <tr>
+              <th>Batch no</th>
+              <th>Name</th>
+              <th>Specification</th>
+              <th>Price</th>
+              <th>Expiry</th>
+              <th>Action</th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+            {medicines?.map((medicine: MedicineType) => (
+              <tr key={medicine.id}>
+                <td>{medicine.batchno}</td>
+                <td>{medicine.name}</td>
+                <td>{medicine.spefication}</td>
+                <td>{medicine.price}</td>
+                <td>{medicine.expiry}</td>
+                <td>
+                  <Flex>
+                    <IconAction
+                      color="green"
+                      icon={<IconEdit size="1.125rem" />}
+                      onClick={() => navigate(`/medicine/${10}`)}
+                    />
+                    <IconAction
+                      color="red"
+                      icon={<IconTrash size="1.125rem" />}
+                      onClick={openDeleteModal}
+                    />
+                  </Flex>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
     </>
   );
 };
