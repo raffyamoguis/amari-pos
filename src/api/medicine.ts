0bb95ecb7a872fca4935e738bba02888b1b1425a
raffyamoguis <raffy.amoguis1@gmail.com>
2023-08-14 21:14:52 +0800

    added uri params

--- src/api/medicine.ts
+++ src/api/medicine.ts
@@ -10,3 +10,27 @@ export async function fetchMedicines(){
         throw error;
     }
 }
+
+export async function searchMedicine(query: string) {
+    if (!!query) {
+        try {
+            const result = await axios.get(`${API_HOST}/products?name=${encodeURIComponent(query)}`);
+            return result.data;
+        }catch (error) {
+            console.log("An error occured on searchMedicine func: ",error);
+            throw error;
+        }
+    }
+}
+
+export async function fetchItemInfo(item: string) {
+    if (!!item) {
+        try {
+            const result = await axios.get(`${API_HOST}/products/${encodeURIComponent(item)}`);
+            return result.data;
+        }catch (error) {
+            console.log("An error occured on fetchItemInfo func: ",error);
+            throw error;
+        }
+    }
+}
