2a8d42c21944760a7bff74e1a4025e8e06eb1f1d
raffyamoguis <raffy.amoguis1@gmail.com>
2023-08-15 16:38:57 +0800

    pagination

--- src/components/stocks/ListStocks.tsx
+++ src/components/stocks/ListStocks.tsx
@@ -1,48 +1,72 @@
 import React from "react";
-import { Table, NumberInput } from "@mantine/core";
+import { shallow } from "zustand/shallow";
+import { useQuery } from "react-query";
+import { Table, NumberInput, Center, Pagination } from "@mantine/core";
+
+import { fetchStocks } from "../../api/stocks";
+import { useStockListStore } from "../../store/useStockListStore";
+import { StockTypes } from "../../types";
 
 import BadgeStock from "../BadgeStock";
+import SkeletonTable from "../skeleton/SkeletonTable";
+
+interface Props {
+  total: number;
+  items: StockTypes[];
+}
 
 const ListStocks: React.FC = () => {
+  const [activePage, offset, setActivePage] = useStockListStore(
+    (state) => [state.activePage, state.offset, state.setActivePage],
+    shallow
+  );
+
+  const { data: stocks, isLoading } = useQuery<Props, Error>(
+    ["medicines", offset],
+    () => fetchStocks(offset),
+    { keepPreviousData: true }
+  );
+
   return (
-    <Table mt={10} fontSize="xs">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Status</th>
-          <th>Quantity</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Neozep</td>
-          <td>
-            <BadgeStock value={100} />
-          </td>
-          <td>
-            <NumberInput defaultValue={110} maw={100} />
-          </td>
-        </tr>
-        <tr>
-          <td>Milk</td>
-          <td>
-            <BadgeStock value={0} />
-          </td>
-          <td>
-            <NumberInput defaultValue={0} maw={100} />
-          </td>
-        </tr>
-        <tr>
-          <td>Choco Drink</td>
-          <td>
-            <BadgeStock value={19} />
-          </td>
-          <td>
-            <NumberInput defaultValue={8} maw={100} />
-          </td>
-        </tr>
-      </tbody>
-    </Table>
+    <>
+      {isLoading ? (
+        <SkeletonTable />
+      ) : (
+        <>
+          <Table mt={10} fontSize="xs">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Status</th>
+                <th>Quantity</th>
+              </tr>
+            </thead>
+            <tbody>
+              {stocks?.items.map((stock: StockTypes) => (
+                <tr key={stock.id}>
+                  <td>{stock.stockfor}</td>
+                  <td>
+                    <BadgeStock value={stock.quantity} />
+                  </td>
+                  <td>
+                    <NumberInput value={stock.quantity} maw={100} />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+          <Center mt={12}>
+            <Pagination
+              size="sm"
+              total={Math.ceil(stocks?.total / 15)}
+              siblings={1}
+              value={activePage}
+              onChange={setActivePage}
+            />
+          </Center>
+        </>
+      )}
+    </>
   );
 };
 
