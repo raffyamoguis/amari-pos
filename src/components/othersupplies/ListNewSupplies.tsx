a239f41aba4efb22b5f1a9f9aff014618172ecb2
raffyamoguis <raffy.amoguis1@gmail.com>
2023-08-15 16:09:15 +0800

    added pagination

--- src/components/othersupplies/ListNewSupplies.tsx
+++ src/components/othersupplies/ListNewSupplies.tsx
@@ -1,14 +1,47 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Group, Table, TextInput, Flex, Text } from "@mantine/core";
+import { useQuery } from "react-query";
+import { shallow } from "zustand/shallow";
+import {
+  Group,
+  Table,
+  TextInput,
+  Flex,
+  Text,
+  Center,
+  Pagination,
+} from "@mantine/core";
 import { notifications } from "@mantine/notifications";
 import { modals } from "@mantine/modals";
 import { IconSearch, IconEdit, IconTrash } from "@tabler/icons-react";
 
+import { fetchOtherSupplies } from "../../api/othersuppliest";
+import { useOtherSuppliesListStore } from "../../store/useOtherSuppliesListStore";
+import { OtherSupplyTypes } from "../../types";
+
 import IconAction from "../IconAction";
+import SkeletonTable from "../skeleton/SkeletonTable";
+
+interface Props {
+  total: number;
+  items: OtherSupplyTypes[];
+}
 
 const ListNewSupplies: React.FC = () => {
   const navigate = useNavigate();
+
+  const [activePage, offset, setActivePage] = useOtherSuppliesListStore(
+    (state) => [state.activePage, state.offset, state.setActivePage],
+    shallow
+  );
+
+  const { data: othersupplies, isLoading } = useQuery<Props, Error>(
+    ["medicines", offset],
+    () => fetchOtherSupplies(offset),
+    { keepPreviousData: true }
+  );
+
+  console.log(othersupplies);
 
   const openDeleteModal = () =>
     modals.openConfirmModal({
@@ -38,39 +71,58 @@ const ListNewSupplies: React.FC = () => {
           icon={<IconSearch size="0.8rem" />}
         />
       </Group>
-      <Table fontSize="xs">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Price</th>
-            <th>Expiry</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Bear brand</td>
-            <td>N/A</td>
-            <td>100</td>
-            <td>Jun 20, 2025</td>
-            <td>
-              <Flex>
-                <IconAction
-                  color="green"
-                  icon={<IconEdit size="1.125rem" />}
-                  onClick={() => navigate(`/othersupplies/${10}`)}
-                />
-                <IconAction
-                  color="red"
-                  icon={<IconTrash size="1.125rem" />}
-                  onClick={openDeleteModal}
-                />
-              </Flex>
-            </td>
-          </tr>
-        </tbody>
-      </Table>
+      {isLoading ? (
+        <SkeletonTable />
+      ) : (
+        <>
+          <Table fontSize="xs">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Price</th>
+                <th>Expiry</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {othersupplies?.items.map((othersupply: OtherSupplyTypes) => (
+                <tr key={othersupply.id}>
+                  <td>{othersupply.name}</td>
+                  <td>{othersupply.description}</td>
+                  <td>{othersupply.price}</td>
+                  <td>{othersupply.expiry}</td>
+                  <td>
+                    <Flex>
+                      <IconAction
+                        color="green"
+                        icon={<IconEdit size="1.125rem" />}
+                        onClick={() =>
+                          navigate(`/othersupplies/${othersupply.id}`)
+                        }
+                      />
+                      <IconAction
+                        color="red"
+                        icon={<IconTrash size="1.125rem" />}
+                        onClick={openDeleteModal}
+                      />
+                    </Flex>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+          <Center mt={12}>
+            <Pagination
+              size="sm"
+              total={Math.ceil(othersupplies?.total / 15)}
+              siblings={1}
+              value={activePage}
+              onChange={setActivePage}
+            />
+          </Center>
+        </>
+      )}
     </>
   );
 };
