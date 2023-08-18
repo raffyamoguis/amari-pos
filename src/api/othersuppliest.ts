b2100c029ee4eb7355f210ef614cb65c3d31a787
raffyamoguis <raffy.amoguis1@gmail.com>
2023-08-18 19:07:56 +0800

    added new fn

--- src/api/othersuppliest.ts
+++ src/api/othersuppliest.ts
@@ -1,6 +1,16 @@
 import axios from "axios";
 import { API_HOST } from "../config";
 
+export async function addOtherSupply(othersupply : any) {
+  try {
+    await axios.post(`${API_HOST}/othersupply`, othersupply);
+    return true; // Return true if the query is successful
+  } catch (error) {
+    console.log("An error occurred in addOtherSupply function:", error);
+    return false; // Return false if the query is unsuccessful
+  }
+}
+
 export async function updateOtherSupply(id: string | undefined, othersupply: any) {
     console.log(othersupply);
     try {
@@ -33,6 +43,16 @@ export async function getOtherSupply(id: string | undefined) {
         return result.data;
     }catch (error) {
         console.log("An error occured on getOtherSupply func: ",error);
+        throw error;
+    }
+}
+
+export async function checkOtherSupply(name: string) {
+    try {
+        const result = await axios.get(`${API_HOST}/othersupply/check/${encodeURIComponent(name)}`);
+        return result.data;
+    }catch (error) {
+        console.log("An error occured on checkMedicine func: ",error);
         throw error;
     }
 }
