306cc7357fdbd97fec6d4895afc2bae93e1f4691
raffyamoguis <raffy.amoguis1@gmail.com>
2023-08-18 18:04:34 +0800

    update

--- src/api/othersuppliest.ts
+++ src/api/othersuppliest.ts
@@ -1,6 +1,17 @@
 import axios from "axios";
 import { API_HOST } from "../config";
 
+export async function updateOtherSupply(id: string | undefined, othersupply: any) {
+    console.log(othersupply);
+    try {
+        await axios.put(`${API_HOST}/othersupply/${id}`, othersupply);
+        return true;
+    }catch (error) {
+        console.log("An error occured on updateOtherSupply func: ",error);
+        throw error;
+    }
+}
+
 export async function fetchOtherSupplies(offset: number, search: string) {
     try {
         if (!!search) {
@@ -12,6 +23,16 @@ export async function fetchOtherSupplies(offset: number, search: string) {
         }
     }catch(error) {
         console.log("An error occured on fetchOthersupplies func: ",error);
+        throw error;
+    }
+}
+
+export async function getOtherSupply(id: string | undefined) {
+    try {
+        const result = await axios.get(`${API_HOST}/othersupply/view/${encodeURIComponent(parseInt(typeof id === "string" ? id : ""))}`);
+        return result.data;
+    }catch (error) {
+        console.log("An error occured on getOtherSupply func: ",error);
         throw error;
     }
 }
--- src/pages/othersupplies/UpdateSupply.tsx
+++ src/pages/othersupplies/UpdateSupply.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import { useQuery, useQueryClient } from "react-query";
 import { notifications } from "@mantine/notifications";
 import {
   Flex,
@@ -10,21 +11,93 @@ import {
   NumberInput,
   Button,
 } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { format } from "date-fns";
 import { DateInput } from "@mantine/dates";
 import { IconChevronLeft } from "@tabler/icons-react";
 
 import IconAction from "../../components/IconAction";
 
-const UpdateSupply: React.FC = () => {
-  const params = useParams();
-  const navigate = useNavigate();
+import { getOtherSupply, updateOtherSupply } from "../../api/othersuppliest";
+import { updateStock } from "../../api/stocks";
 
-  console.log(params.id);
-  const handleUpdate = () => {
-    notifications.show({
-      message: "Successfully updated.",
-      color: "green",
+const UpdateSupply: React.FC = () => {
+  const queryClient = useQueryClient();
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const form = useForm({
+    initialValues: {
+      name: "",
+      description: "",
+      expiry: null as Date | null | "",
+      price: 0,
+    },
+
+    validate: {
+      name: (value) => (value.length === 0 ? "Name is required" : null),
+      price: (value) => {
+        if (value.toString.length === 0) {
+          return "Price is required";
+        }
+        return null;
+      },
+    },
+  });
+
+  const { data: othersupply, isSuccess } = useQuery("othersupply", () =>
+    getOtherSupply(id)
+  );
+
+  useEffect(() => {
+    if (isSuccess && othersupply) {
+      form.setValues({
+        name: othersupply?.name,
+        description: othersupply?.description,
+        expiry: othersupply?.expiry ? new Date(othersupply?.expiry) : null,
+        price: othersupply?.price ? parseFloat(othersupply?.price) : 0,
+      });
+    }
+  }, [isSuccess, othersupply]);
+
+  const handleUpdate = async (values: any) => {
+    setLoading(true); // Set laoding
+    values.name = values.name.trim(); // Remove trailing spaces
+    const { name } = values;
+
+    if (name !== othersupply?.name) {
+      await updateStock(othersupply?.name, name);
+      await queryClient.invalidateQueries("stocks");
+    }
+
+    // Formate date value
+    let newExpiry;
+    if (!!values.expiry) {
+      const expiry = new Date(values.expiry);
+      newExpiry = format(expiry, "yyyy-MMMM-dd");
+    }
+
+    if (values.expiry === null) {
+      newExpiry = "";
+    }
+
+    const isUpdateOtherSupplySuccess = await updateOtherSupply(id, {
+      name: values.name,
+      description: values.description,
+      price: values.price,
+      expiry: newExpiry,
     });
+
+
+    if (isUpdateOtherSupplySuccess) {
+      notifications.show({
+        message: "Successfully updated.",
+        color: "green",
+      });
+      await queryClient.invalidateQueries("othersupplies");
+      setLoading(false);
+    }
   };
   return (
     <>
@@ -38,32 +111,37 @@ const UpdateSupply: React.FC = () => {
         <Text>Update Other Supply</Text>
       </Flex>
       <Center mt={20}>
-        <Stack align="stretch" w={350}>
-          <TextInput
-            label="Name"
-            placeholder="Enter name"
-            defaultValue="Neozep"
-          />
-          <TextInput
-            label="Description"
-            placeholder="Enter description"
-            defaultValue="N/A"
-          />
-          <DateInput
-            label="Expiry date"
-            valueFormat="YYYY MMM DD"
-            placeholder="Set expiry date"
-            clearable
-          />
-          <NumberInput
-            label="Price"
-            placeholder="Enter price"
-            defaultValue={100}
-          />
-          <Button color="green" onClick={handleUpdate}>
-            Update
-          </Button>
-        </Stack>
+        <form onSubmit={form.onSubmit((values) => handleUpdate(values))}>
+          <Stack align="stretch" w={350}>
+            <TextInput
+              label="Name"
+              placeholder="Enter name"
+              {...form.getInputProps("name")}
+            />
+            <TextInput
+              label="Description"
+              placeholder="Enter description"
+              {...form.getInputProps("description")}
+            />
+            <DateInput
+              label="Expiry date"
+              valueFormat="YYYY MMM DD"
+              placeholder="Set expiry date"
+              {...form.getInputProps("expiry")}
+              clearable
+            />
+            <NumberInput
+              label="Price"
+              placeholder="Enter price"
+              precision={2}
+              min={1}
+              {...form.getInputProps("price")}
+            />
+            <Button color="green" type="submit" loading={loading}>
+              Update
+            </Button>
+          </Stack>
+        </form>
       </Center>
     </>
   );
