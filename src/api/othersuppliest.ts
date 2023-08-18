dadcf898b7dc6cb7dbda191ca62cc4d3d1ff68b9
raffyamoguis <raffy.amoguis1@gmail.com>
2023-08-18 16:08:24 +0800

    added delete fn

--- src/api/othersuppliest.ts
+++ src/api/othersuppliest.ts
@@ -10,3 +10,13 @@ export async function fetchOtherSupplies(offset: number) {
         throw error;
     }
 }
+
+export async function deleteOtherSupply(id: number) {
+    try {
+        const result = await axios.delete(`${API_HOST}/othersupply/${encodeURIComponent(id)}`);
+        return result.data;
+    }catch (error) {
+        console.log("An error occured on deleteOtherSupply func: ",error);
+        throw error;
+    }
+}
