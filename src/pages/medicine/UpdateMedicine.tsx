63378993fd8a01517c0e9b84fea7cf777cec4801
raffyamoguis <raffy.amoguis1@gmail.com>
2023-08-18 11:57:57 +0800

    added update functionality

--- src/pages/medicine/UpdateMedicine.tsx
+++ src/pages/medicine/UpdateMedicine.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import { useQuery, useQueryClient } from "react-query";
 import { notifications } from "@mantine/notifications";
 import {
   Flex,
@@ -10,23 +11,95 @@ import {
   NumberInput,
   Button,
 } from "@mantine/core";
+import { useForm } from "@mantine/form";
 import { DateInput } from "@mantine/dates";
+import { format } from "date-fns";
 import { IconChevronLeft } from "@tabler/icons-react";
 
 import IconAction from "../../components/IconAction";
+import SkeletonTable from "../../components/skeleton/SkeletonTable";
+
+import { getMedicine, updateMedicine } from "../../api/medicine";
+import { updateStock } from "../../api/stocks";
 
 const UpdateMedicine: React.FC = () => {
-  const params = useParams();
+  const queryClient = useQueryClient();
+  const { id } = useParams();
   const navigate = useNavigate();
+  const [loading, setLoading] = useState<boolean>(false);
 
-  console.log(params.id);
-  const handleUpdate = () => {
-    notifications.show({
-      message: "Successfully updated.",
-      color: "green",
+  const form = useForm({
+    initialValues: {
+      batchno: "",
+      name: "",
+      specification: "",
+      expiry: null as Date | null | "",
+      price: 0,
+    },
+
+    validate: {
+      name: (value) => (value.length === 0 ? "Name is required" : null),
+      price: (value) => {
+        if (value.toString.length === 0) {
+          return "Price is required";
+        }
+        return null;
+      },
+    },
+  });
+
+  const {
+    data: medicine,
+    isSuccess,
+    isLoading,
+  } = useQuery("medicine", () => getMedicine(id));
+
+  useEffect(() => {
+    if (isSuccess && medicine) {
+      form.setValues({
+        batchno: medicine?.batchno,
+        name: medicine?.name,
+        specification: medicine?.specification,
+        expiry: medicine?.expiry ? new Date(medicine?.expiry) : null,
+        price: medicine?.price ? parseFloat(medicine?.price) : 0,
+      });
+    }
+  }, [isSuccess, medicine]);
+
+  const handleUpdate = async (values: any) => {
+    setLoading(true); // Set laoding
+    values.name = values.name.trim(); // Remove trailing spaces
+    const { name } = values;
+
+    if (name !== medicine?.name) {
+      await updateStock(medicine?.name, name);
+      await queryClient.invalidateQueries("stocks");
+    }
+
+    // Formate date value
+    let newExpiry;
+    if (values.expiry !== "") {
+      const expiry = new Date(values.expiry);
+      newExpiry = format(expiry, "yyyy-MMMM-dd");
+    }
+
+    const isUpdateMedSuccess = await updateMedicine(id, {
+      batchno: values.batchno,
+      name: values.name,
+      specification: values.specification,
+      price: values.price,
+      expiry: newExpiry,
     });
-  };
 
+    if (isUpdateMedSuccess) {
+      notifications.show({
+        message: "Successfully updated.",
+        color: "green",
+      });
+      await queryClient.invalidateQueries("medicines");
+      setLoading(false);
+    }
+  };
   return (
     <>
       <Flex align="center">
@@ -39,37 +112,46 @@ const UpdateMedicine: React.FC = () => {
         <Text>Update Medicine</Text>
       </Flex>
       <Center mt={20}>
-        <Stack align="stretch" w={350}>
-          <TextInput
-            label="Batch no"
-            placeholder="Enter batch no"
-            defaultValue="1112"
-          />
-          <TextInput
-            label="Name"
-            placeholder="Enter name"
-            defaultValue="Neozep"
-          />
-          <TextInput
-            label="Specification"
-            placeholder="Enter specification"
-            defaultValue="N/A"
-          />
-          <DateInput
-            label="Expiry date"
-            valueFormat="YYYY MMM DD"
-            placeholder="Set expiry date"
-            clearable
-          />
-          <NumberInput
-            label="Price"
-            placeholder="Enter price"
-            defaultValue={100}
-          />
-          <Button color="green" onClick={handleUpdate}>
-            Update
-          </Button>
-        </Stack>
+        {isLoading ? (
+          <SkeletonTable />
+        ) : (
+          <form onSubmit={form.onSubmit((values) => handleUpdate(values))}>
+            <Stack align="stretch" w={350}>
+              <TextInput
+                label="Batch no"
+                placeholder="Enter batch no"
+                {...form.getInputProps("batchno")}
+              />
+              <TextInput
+                label="Name"
+                placeholder="Enter name"
+                {...form.getInputProps("name")}
+              />
+              <TextInput
+                label="Specification"
+                placeholder="Enter specification"
+                {...form.getInputProps("specification")}
+              />
+              <DateInput
+                label="Expiry"
+                valueFormat="YYYY-MMMM-DD"
+                placeholder="Set expiry date"
+                clearable
+                {...form.getInputProps("expiry")}
+              />
+              <NumberInput
+                label="Price"
+                placeholder="Enter price"
+                precision={2}
+                min={1}
+                {...form.getInputProps("price")}
+              />
+              <Button color="green" type="submit" loading={loading}>
+                Update
+              </Button>
+            </Stack>
+          </form>
+        )}
       </Center>
     </>
   );
