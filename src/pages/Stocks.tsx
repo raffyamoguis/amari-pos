d405d4f90001bb35b9558bb97c3c6650f745832c
raffyamoguis <raffy.amoguis1@gmail.com>
2023-08-18 13:53:57 +0800

    added search functionality

--- src/pages/Stocks.tsx
+++ src/pages/Stocks.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { TextInput, Text } from "@mantine/core";
-import { IconSearch } from "@tabler/icons-react";
+import { IconSearch, IconX } from "@tabler/icons-react";
 
 import ListStocks from "../components/stocks/ListStocks";
+import { useStockListStore } from "../store/useStockListStore";
 
 const Stocks: React.FC = () => {
+  const [search, setSearch, setActivePage] = useStockListStore((state) => [
+    state.search,
+    state.setSearch,
+    state.setActivePage,
+  ]);
+
   return (
     <>
       <Text mb={10}>Manage stocks</Text>
@@ -12,6 +19,19 @@ const Stocks: React.FC = () => {
         placeholder="Enter stock name"
         maw={300}
         icon={<IconSearch size="0.80rem" />}
+        rightSection={
+          search !== "" && (
+            <IconX
+              size="0.8rem"
+              onClick={() => {
+                setSearch("");
+                setActivePage(1);
+              }}
+            />
+          )
+        }
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
       <ListStocks />
     </>
