72293377363762ff66c4c42b80d19faff5af40cd
raffyamoguis <raffy.amoguis1@gmail.com>
2023-08-18 13:30:33 +0800

    added all functionality

--- src/components/medicine/ListMedicine.tsx
+++ src/components/medicine/ListMedicine.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 import { shallow } from "zustand/shallow";
 import {
   Group,
@@ -13,9 +13,10 @@ import {
 } from "@mantine/core";
 import { modals } from "@mantine/modals";
 import { notifications } from "@mantine/notifications";
-import { IconSearch, IconEdit, IconTrash } from "@tabler/icons-react";
+import { IconSearch, IconEdit, IconTrash, IconX } from "@tabler/icons-react";
 
-import { fetchMedicines } from "../../api/medicine";
+import { fetchMedicines, deleteMedicine } from "../../api/medicine";
+import { deleteStock } from "../../api/stocks";
 import { useMedicineListStore } from "../../store/useMedicineListStore";
 import { MedicineType } from "../../types";
 
@@ -29,36 +30,76 @@ interface Props {
 
 const ListMedicine: React.FC = () => {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
-  const [activePage, offset, setActivePage] = useMedicineListStore(
-    (state) => [state.activePage, state.offset, state.setActivePage],
-    shallow
-  );
+  const [activePage, offset, search, setActivePage, setSearch] =
+    useMedicineListStore(
+      (state) => [
+        state.activePage,
+        state.offset,
+        state.search,
+        state.setActivePage,
+        state.setSearch,
+      ],
+      shallow
+    );
 
   const { data: medicines, isLoading } = useQuery<Props, Error>(
-    ["medicines", offset],
-    () => fetchMedicines(offset),
+    ["medicines", offset, search],
+    () => fetchMedicines(offset, search),
     { keepPreviousData: true }
   );
 
-  const openDeleteModal = () =>
+  useEffect(() => {
+    if (search !== "") {
+      setActivePage(1);
+    }
+  }, [search]);
+
+  const openDeleteModal = (id: number | undefined, name: string) =>
     modals.openConfirmModal({
       title: "Confirm delete",
       centered: true,
       children: (
         <Text size="sm">
-          Are you sure you want to delete this medicine? This action cannot be
-          reverted once the data is deleted it cannot be restored again.
+          Are you sure you want to delete{" "}
+          <Text component="span" fw={700}>
+            {name}
+          </Text>
+          ? All of its stocks will also be deleted.
         </Text>
       ),
       labels: { confirm: "Delete", cancel: "Cancel" },
       confirmProps: { color: "red" },
       onCancel: () => console.log("Cancel"),
-      onConfirm: () => {
-        notifications.show({
-          message: "Successfully deleted.",
-          color: "red",
-        });
+      onConfirm: async () => {
+        const isDeleteMedSuccess = await deleteMedicine(id);
+
+        if (isDeleteMedSuccess) {
+          //Remove the stock
+          const isRemoveStockSuccess = await deleteStock(name);
+
+          if (isRemoveStockSuccess) {
+            notifications.show({
+              message: "Successfully deleted.",
+              color: "success",
+            });
+
+            // Invalidate queries
+            await queryClient.invalidateQueries("medicines");
+            await queryClient.invalidateQueries("stocks");
+          } else {
+            notifications.show({
+              message: "There is an error deleting this data.",
+              color: "red",
+            });
+          }
+        } else {
+          notifications.show({
+            message: "There is an error deleting this data.",
+            color: "red",
+          });
+        }
       },
     });
   return (
@@ -67,54 +108,76 @@ const ListMedicine: React.FC = () => {
         <TextInput
           placeholder="Enter medicine name"
           icon={<IconSearch size="0.8rem" />}
+          rightSection={
+            search !== "" && (
+              <IconX
+                size="0.8rem"
+                onClick={() => {
+                  setSearch("");
+                  setActivePage(1);
+                }}
+              />
+            )
+          }
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
         />
       </Group>
       {isLoading ? (
         <SkeletonTable />
       ) : (
         <>
-          <Table fontSize="xs">
-            <thead>
-              <tr>
-                <th>Batch no</th>
-                <th>Name</th>
-                <th>Specification</th>
-                <th>Price</th>
-                <th>Expiry</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {medicines?.items.map((medicine: MedicineType) => (
-                <tr key={medicine.id}>
-                  <td>{medicine.batchno}</td>
-                  <td>{medicine.name}</td>
-                  <td>{medicine.spefication}</td>
-                  <td>{medicine.price}</td>
-                  <td>{medicine.expiry}</td>
-                  <td>
-                    <Flex>
-                      <IconAction
-                        color="green"
-                        icon={<IconEdit size="1.125rem" />}
-                        onClick={() => navigate(`/medicine/${10}`)}
-                      />
-                      <IconAction
-                        color="red"
-                        icon={<IconTrash size="1.125rem" />}
-                        onClick={openDeleteModal}
-                      />
-                    </Flex>
-                  </td>
+          {medicines?.total === 0 ? (
+            <Center mt={20}>
+              <Text fz="sm">No results..</Text>
+            </Center>
+          ) : (
+            <Table fontSize="xs">
+              <thead>
+                <tr>
+                  <th>Batch no</th>
+                  <th>Name</th>
+                  <th>Specification</th>
+                  <th>Price</th>
+                  <th>Expiry</th>
+                  <th>Action</th>
                 </tr>
-              ))}
-            </tbody>
-          </Table>
+              </thead>
+              <tbody>
+                {medicines?.items.map((medicine: MedicineType) => (
+                  <tr key={medicine.id}>
+                    <td>{medicine.batchno}</td>
+                    <td>{medicine.name}</td>
+                    <td>{medicine.specification}</td>
+                    <td>{medicine.price}</td>
+                    <td>{medicine.expiry}</td>
+                    <td>
+                      <Flex>
+                        <IconAction
+                          color="green"
+                          icon={<IconEdit size="1.125rem" />}
+                          onClick={() => navigate(`/medicine/${medicine.id}`)}
+                        />
+
+                        <IconAction
+                          color="red"
+                          icon={<IconTrash size="1.125rem" />}
+                          onClick={() =>
+                            openDeleteModal(medicine?.id, medicine.name)
+                          }
+                        />
+                      </Flex>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          )}
 
           <Center mt={12}>
             <Pagination
               size="sm"
-              total={Math.ceil(medicines.total / 15)}
+              total={medicines?.total ? Math.ceil(medicines.total / 15) : 0}
               siblings={1}
               value={activePage}
               onChange={setActivePage}
