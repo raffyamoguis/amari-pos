151830cee5b517ffdd3f56f284fd93ed35a22b0e
raffyamoguis <raffy.amoguis1@gmail.com>
2023-08-18 11:59:00 +0800

    added update med

--- src/api/medicine.ts
+++ src/api/medicine.ts
@@ -1,6 +1,42 @@
 import axios from "axios";
 import { API_HOST } from "../config";
 
+export async function addMedicine(medicine : any) {
+  try {
+    await axios.post(`${API_HOST}/medicine`, medicine);
+    return true; // Return true if the query is successful
+  } catch (error) {
+    console.log("An error occurred in addMedicine function:", error);
+    return false; // Return false if the query is unsuccessful
+  }
+}
+
+export async function getMedicine(id : string | undefined) {
+  try {
+    const result = await axios.get(`${API_HOST}/medicine/${parseInt(typeof id === "string" ? id : "")}`);
+    return result?.data;
+  } catch (error) {
+    console.log("An error occurred in addMedicine function:", error);
+    return false; // Return false if the query is unsuccessful
+  }
+}
+
+export async function updateMedicine(id: string | undefined, medicine : any) {
+  try {
+    await axios.put(`${API_HOST}/medicine/${id}`, {
+      batchno: medicine.batchno,
+      name: medicine.name,
+      specification: medicine.specification,
+      price: medicine.price,
+      expiry: medicine.expiry
+    });
+    return true; // Return true if the query is successful
+  } catch (error) {
+    console.log("An error occurred in updateMedicine function:", error);
+    throw error;// Return false if the query is unsuccessful
+  }
+}
+
 export async function fetchMedicines(offset: number){
     try {
         const result = await axios.get(`${API_HOST}/medicine?offset=${offset}`);
@@ -33,4 +69,24 @@ export async function fetchItemInfo(item: string) {
             throw error;
         }
     }
+}
+
+export async function checkMedicine(name: string) {
+    try {
+        const result = await axios.get(`${API_HOST}/searchRedundant/${encodeURIComponent(name)}`);
+        return result.data;
+    }catch (error) {
+        console.log("An error occured on checkMedicine func: ",error);
+        throw error;
+    }
+}
+
+export async function deleteMedicine(id: number) {
+    try {
+        await axios.delete(`${API_HOST}/medicine/${id}`);
+        return true;
+    }catch (error) {
+        console.log("An error occurred in deleteMedicine function:", error);
+        return false; // Return false if the query is unsuccessful
+  }
 }
