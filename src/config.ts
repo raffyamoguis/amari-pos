df705419a75fbc89fea14b14002416fa5cdb8881
raffyamoguis <raffy.amoguis1@gmail.com>
2023-09-06 09:43:28 +0800

    added analytics

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,3 @@
 export const API_HOST = import.meta.env.VITE_API_HOST;
+
+export const MEASUREMENT_ID = import.meta.env.VITE_MEASUREMENT_ID;
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App.tsx";
 import "./index.css";
+import { MEASUREMENT_ID } from "./config.ts";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize(MEASUREMENT_ID);
+
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
 
 const queryClient = new QueryClient();
 
