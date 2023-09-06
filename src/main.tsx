73fb3da40e954083964e4824d8e9ef3565634429
raffyamoguis <86818651+raffyamoguis@users.noreply.github.com>
2023-09-06 12:40:10 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,15 @@ import ReactGA from "react-ga4";
 
 ReactGA.initialize(MEASUREMENT_ID);
 
-ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+// Detect the device type dynamically
+const deviceType = window.innerWidth < 768 ? 'mobile' : 'desktop'; // Example: Consider devices with a screen width less than 768px as "mobile"
+
+// Set the device_type user property
+ReactGA.gtag('set', { 'device_type': deviceType });
+
+ReactGA.send({ hitType: "pageview", page: window.location.pathname, user_properties: {
+    'device_type': deviceType
+  } });
 
 const queryClient = new QueryClient();
 
